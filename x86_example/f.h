419785a02134b5581d7f8d845ca94999ce33e92a
Michal Kowalczyk <mpkowalcz@wp.pl>
2021-06-15 01:25:41 +0200

    Deleted unnecessary files

--- x86_example/f.h
+++
@@ -1,7 +0,0 @@
-#ifndef F_H_
-#define F_H_
-
-void f(char *);
-
-#endif // F_H_
-
--- x86_project/f.h
+++
@@ -1,7 +0,0 @@
-#ifndef F_H_
-#define F_H_
-
-void drawQuadratic(unsigned char * pPixelBuffer, int width, int height, int a, int b, int c, int S);
-
-#endif // F_H_
-
