cfad9d4243646e57b6d192d5351cc64ee7f08a7e
Michal Kowalczyk <mpkowalcz@wp.pl>
2021-06-14 23:14:57 +0200

    Start of work on assembler compatibility

--- x86_project/main.c
+++ x86_project/main.c
@@ -4,7 +4,7 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
-#include "allegro5/allegro_ttf.h"
+#include <allegro5/allegro_ttf.h>
 
 enum chosenParameter{A, B, C, D, None};
 
